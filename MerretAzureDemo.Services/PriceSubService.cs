0164865ed18da631a69225f7d0a5b0a7683d493c
Tun Hein <tun.hein@harveynichols.com>
2018-03-06 14:36:24 +0000

    - added useful comments.

--- MerretAzureDemo.Services/PriceSubService.cs
+++ MerretAzureDemo.Services/PriceSubService.cs
@@ -52,8 +52,11 @@ namespace MerretAzureDemo.Services
 
         void RegisterOnMessageHandlerAndReceiveMessages()
         {
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
             var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
             {
+                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
                 MaxConcurrentCalls = 1,
 
                 // Indicates whether the message pump should automatically complete the messages after returning from user callback.
